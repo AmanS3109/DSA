3eae6e05cbc46519b1fd32cf61006b7275cbb124
AmanS3109 <singhaman2122@ternaengg.ac.in>
2024-06-06 16:03:05 +0530

    commit message

--- twoSum.java
+++ twoSum.java
@@ -2,18 +2,19 @@ import java.util.HashMap;
 
 public class twoSum {
     public static void main(String[] args) {
-        
+        int[] book = {2, 3, 4, 5, 6, 7, 8, 11};
+        System.out.println(findSum(8, book, 14));
     }
-    static String findSum(int n, int[] arr, int target){
+    static String findSum(int n, int[] book, int target){
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < arr.length; i++) {
-            int firstNum = arr[i];
+        for (int i = 0; i < book.length; i++) {
+            int firstNum = book[i];
             int secNum = target - firstNum;
-            if (map.find(secNum) != map.end()) {
-                return "Yes";
+            if (map.containsKey(secNum)) {
+                return "YES";
             }
-            map[firstNum] = i; 
+            map.put(firstNum, i);
         }
-        return "No";
+        return "NO";
     }
 }
